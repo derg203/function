3b49dc1feeb81795fe8d0aa29ee8d975a5f02142
derg203 <ddergacev714@gmail.com>
2024-10-20 18:45:24 +0300

    Add files via upload

---
+++ функция.py
@@ -0,0 +1,10 @@
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([]) # добавляем список
+        for j in range(m):
+            matrix[i].append(value) # Пишем в список значение
+    print(matrix)
+get_matrix(2, 2, 10)
+get_matrix(3, 5, 42)
+get_matrix(4, 2, 13)
